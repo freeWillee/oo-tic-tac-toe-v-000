4267629e639aac25695a811cf399929dd7a3a392
Will Lee <will.i.lee@outlook.com>
2018-05-09 19:31:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,5 +3,14 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-
+  WIN_COMBINATIONS = [
+    [0,1,2], #top row
+    [3,4,5], #middle row
+    [6,7,8], #bottom row
+    [0,3,6], #first column
+    [1,4,7], #second column
+    [2,5,8], #third column
+    [0,4,8], #back-slash
+    [2,4,6] #forward-slash
+  ]
 end
