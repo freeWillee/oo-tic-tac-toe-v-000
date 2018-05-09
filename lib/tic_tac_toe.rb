293569b61067c86208daf989c9506e84e1d3404a
Will Lee <will.i.lee@outlook.com>
2018-05-09 19:38:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,4 +74,44 @@ def turn_count(@board)
   return i
 end
 
+#The #current_player method should take in an argument of the game board and use the #turn_count method to determine if it is "X"'s turn or "O"'s.
+def current_player(@board)
+  if turn_count(@board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def won?()
+  #iterate though the constant, WIN_COMBINATIONS
+  WIN_COMBINATIONS.each do |win_combo|
+    #Define index position on 'board' to be tested;
+    index_1 = win_combo[0]
+    index_2 = win_combo[1]
+    index_3 = win_combo[2]
+
+    # assign win_combo indices
+    pos_1 = @board[index_1]
+    pos_2 = @board[index_2]
+    pos_3 = @board[index_3]
+
+    # test to see if win_combo is on the 'board'
+    if (pos_1 == "X" && pos_2 == "X" && pos_3 == "X") ||
+      (pos_1 == "O" && pos_2 == "O" && pos_3 == "O")
+      # return the win_combo array if true and break out of the loop and method
+      return win_combo
+    end
+  end
+  # if there are no win_combinations, then return false and break out of the method
+  return false
+end
+
+def full?()
+  #The #full? method should accept a board and return true if every element in the board contains either an "X" or an "O".
+  @board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
+
 end
