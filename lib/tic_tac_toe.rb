4d1e17ea6e276a43fded86c63b2efbaaffc5a82d
Will Lee <will.i.lee@outlook.com>
2018-05-09 19:35:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,4 +49,19 @@ def valid_move?(@board, index)
   end
 end
 
+#define the turn method.
+def turn(@board)
+  puts "Please enter 1-9:"
+  index = input_to_index(gets.strip)
+  if valid_move?(@board, index)
+    move(board, index, current_player(@board))
+    display_board(@board)
+  else
+    #binding.pry
+    puts "That was an invalid entry."
+    turn(@board)
+
+  end
+end
+
 end
